1c9cec58a2a446cf511f4f1743e5767ffc6c9c67
[huangketong] <huangkt@joyowo.com>
2016-12-01 14:15:48 +0800

    修改start

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ var webpack = require('webpack');
 module.exports = {
     entry: './entry.js',
     output: {
-        path: __dirname,
-        filename: 'bundle.js'
+        path: __dirname, //__dirname是node.js的一个全局变量，它指向当前执行脚本所在的目录
+        filename: 'bundle.js' //打包后输出文件的文件名
     },
     module: {
         loaders: [
