7d93f1f930332a5bb84209f4f80caa964833668e
[huangketong] <huangkt@joyowo.com>
2016-12-02 15:44:34 +0800

    second

--- entry.js
+++ entry.js
@@ -1,5 +1,21 @@
 require('./style.css');
+var config = require('./config.json');
+// var greet = document.createElement('div');
+// greet.textContent = config.greetText;
+
+document.write(config.greetText);
 
 document.write('It works...');
 var str = require('./module.js');
 document.write(str);
+
+import React, { Component } from 'react'
+import config from './config.json';
+
+class Greeter extends Component {
+    render() {
+        return ('<div>${config.greetText}</div>');
+    }
+}
+
+export default Greeter
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,32 @@ var webpack = require('webpack');
 
 
 module.exports = {
+    devtool: 'eval-source-map', //配置生成Source Maps,选择合适的选项
     entry: './entry.js',
     output: {
         path: __dirname, //__dirname是node.js的一个全局变量，它指向当前执行脚本所在的目录
-        filename: 'bundle.js' //打包后输出文件的文件名
+
+        filename: 'bundle.js' // 打包后输出文件的文件名
+    },
+
+    devServer: {
+        contentBase: "./", //本地服务器所加载的页面所在的目录
+        color: true, //终端中输出结果为彩色
+        historyApiFallback: true, //不跳转
+        inline: true //实时刷新
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: 'style!css' }
+            { test: /\.css$/, loader: 'style!css' },
+            { test: /\.json$/, loader: 'json' },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'bable',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
         ]
     },
     plugin: [
