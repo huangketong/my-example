c0edd8a939a56b46e81101f729f0aca4729c106c
[huangketong] <huangkt@joyowo.com>
2016-12-05 11:10:15 +0800

    update

--- function/1.js
+++ function/1.js
@@ -9,16 +9,20 @@ $.each(str, function(index, value) {
 
 var elem = document.getElementsByTagName('div');
 
+// 由于点击任何一个div事件时，i的值已经被赋值为5
 // for (var i = 0; i < elem.length; i++) {
 //     elem[i].onclick = function() {
 //         alert(i);
 //     }
 // }
 
+
+
+// 在绑定点击事件外部封装一个立即执行函数，并将i传入该函数
 for (var i = 0; i < elem.length; i++) {
-    (function(i) {
-        elem[i].onclick = function() {
-            alert(i);
+    (function(n) {
+        elem[n].onclick = function() {
+            alert(n);
         }
     })(i);
 }
