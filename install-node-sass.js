b9b5bfc1ed96e399b66559e682cf3a39f2b9549c
huangketong <18790506726@163.com>
2017-04-01 00:04:48 +0800

    增加.npmrc方法

--- install-node-sass.js
+++ install-node-sass.js
@@ -1,7 +1,17 @@
-node-sass 安装失败？ 处理方法 1 2 3 
+node - sass 安装失败？ 处理方法 1 2 3
 1.
 npm install cnpm
 2.
-cnpm intall node-sass
+cnpm install node - sass
 3.
-cnpm intall sass-loader
+cnpm install sass - loader
+
+
+two:
+    在项目目录下建一个.npmrc文件
+这样就可以没有安转node - sass自动安装淘宝镜像源，
+建议安转后直接删掉这个文件（ 不要问为什么？）， 以免影响后续的pull..
+sass_binary_site = https: //npm.taobao.org/mirrors/node-sass/
+    phantomjs_cdnurl = http: //cnpmjs.org/downloads
+    electron_mirror = https: //npm.taobao.org/mirrors/electron/
+    registry = https: //registry.npm.taobao.org
